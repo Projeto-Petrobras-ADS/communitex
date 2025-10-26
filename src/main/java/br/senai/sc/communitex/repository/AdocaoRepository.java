93361af629eacdeefeb2b9d2171ddec7c9e1075b
Samoel <samoel.jrr@gmail.com>
2025-10-26 20:21:17 -0300

    Novos Gets para adoções

--- src/main/java/br/senai/sc/communitex/repository/AdocaoRepository.java
+++ src/main/java/br/senai/sc/communitex/repository/AdocaoRepository.java
@@ -3,11 +3,27 @@ package br.senai.sc.communitex.repository;
 import br.senai.sc.communitex.enums.StatusAdocao;
 import br.senai.sc.communitex.model.Adocao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface AdocaoRepository extends JpaRepository<Adocao, Long> {
     List<Adocao> findByStatus(StatusAdocao adocao);
 
+    List<Adocao> findByPraca(Long pracaId);
+
     List<Adocao> findByEmpresaId(Long empresaId);
+
+    @Query("SELECT a FROM Adocao a WHERE a.dataInicio >= :inicio AND a.dataFIM <= :fim")
+    List<Adocao> findByPeriodo(@Param("inicio") LocalDate inicio, @Param("fim") LocalDate fim);
+
+
+    @Query("""
+            SELECT a FROM Adocao a\s
+            WHERE (:status IS NULL OR a.status = :status)
+            AND a.dataFim BETWEEN :hoje AND :limite
+           \s""")
+    List<Adocao> findAdocoesByPrazoEStatus(@Param("hoje") LocalDate hoje, @Param("limite") LocalDate limite, @Param("status") StatusAdocao status);
 }
--- src/main/java/br/senai/sc/communitex/service/AdocaoService.java
+++ src/main/java/br/senai/sc/communitex/service/AdocaoService.java
@@ -3,6 +3,7 @@ package br.senai.sc.communitex.service;
 
 import br.senai.sc.communitex.dto.AdocaoRequestDTO;
 import br.senai.sc.communitex.dto.AdocaoResponseDTO;
+import br.senai.sc.communitex.dto.AdocaoStatusResponseDTO;
 import br.senai.sc.communitex.enums.StatusAdocao;
 import br.senai.sc.communitex.enums.StatusPraca;
 import br.senai.sc.communitex.exception.InvalidAdocaoException;
@@ -75,6 +76,58 @@ public class AdocaoService {
 
         return toResponseDTO(adocao);
 
+    }
+
+    public List<AdocaoStatusResponseDTO> findByStatus(StatusAdocao status){
+        List<Adocao> adocaos = adocaoRepository.findByStatus(status);
+
+        return adocaos.stream()
+                .map(adocao -> new AdocaoStatusResponseDTO(
+                        adocao.getId(),
+                        adocao.getDescricaoProjeto(),
+                        adocao.getDataInicio(),
+                        adocao.getDataFim(),
+                        adocao.getStatus(),
+                        adocao.getEmpresa() != null ? adocao.getEmpresa().getNomeFantasia() : null,
+                        adocao.getPraca() != null ? adocao.getPraca().getNome() : null
+                )).collect(Collectors.toList());
+
+    }
+
+    public List<AdocaoResponseDTO> findByPeriodo(LocalDate inicio, LocalDate fim) {
+        if(inicio == null || fim == null) {
+            throw new IllegalStateException("As datas de inicio e fim são obrigatorios");
+        }
+
+        if(fim.isBefore(inicio)){
+            throw new IllegalStateException("A data final não pode ser anterior à data inicial");
+        }
+
+        List<Adocao> adocaos = adocaoRepository.findByPeriodo(inicio, fim);
+
+        return  adocaos.stream().map(this::toResponseDTO).toList();
+    }
+
+    public List<AdocaoResponseDTO> findByEmpresa(Long empresaId) {
+        List<Adocao> adocaos = adocaoRepository.findByEmpresaId(empresaId);
+        return adocaos.stream().map(this::toResponseDTO).toList();
+    }
+
+    public List<AdocaoResponseDTO> findAdocoesByPrazoEStatus(Integer dias, StatusAdocao status){
+        if(dias == null || dias <= 0){
+            dias = 7;
+        }
+        LocalDate hoje = LocalDate.now();
+        LocalDate limite = hoje.plusDays(dias);
+
+        List<Adocao> adocaos = adocaoRepository.findAdocoesByPrazoEStatus(hoje, limite, status);
+        return adocaos.stream().map(this::toResponseDTO).toList();
+
+    }
+
+    public List<AdocaoResponseDTO> findByPraca(Long pracaId) {
+        List<Adocao> adocaos = adocaoRepository.findByPraca(pracaId);
+        return adocaos.stream().map(this::toResponseDTO).toList();
     }
 
     public AdocaoResponseDTO update(Long id, AdocaoRequestDTO dto){
