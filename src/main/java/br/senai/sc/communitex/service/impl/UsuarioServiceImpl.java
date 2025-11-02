deaa7d37107670df180e9b5a123e0d5cc8086e54
Murilo da Silva <murilodasilva@MacBook-Pro-de-Murilo.local>
2025-11-02 11:45:27 -0300

    adding user service

--- src/main/java/br/senai/sc/communitex/service/impl/UsuarioServiceImpl.java
+++ src/main/java/br/senai/sc/communitex/service/impl/UsuarioServiceImpl.java
@@ -3,12 +3,15 @@ package br.senai.sc.communitex.service.impl;
 import br.senai.sc.communitex.model.Usuario;
 import br.senai.sc.communitex.repository.UsuarioRepository;
 import br.senai.sc.communitex.service.UsuarioService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-public class UsuarioServiceImpl implements UsuarioService {
+public class UsuarioServiceImpl implements UsuarioService, UserDetailsService {
 
     private final UsuarioRepository usuarioRepository;
 
@@ -21,7 +24,14 @@ public class UsuarioServiceImpl implements UsuarioService {
         return usuarioRepository.findByUsername(username);
     }
 
+    @Override
     public Usuario save(Usuario usuario) {
         return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + username));
     }
 }
