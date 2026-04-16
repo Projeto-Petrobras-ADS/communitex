100637d000409813a6818b3407b6f7890465d22e
Murilo da Silva <mrl.dsilva@gmail.com>
2026-04-16 20:57:59 -0300

    vercel

--- src/main/java/br/senai/sc/communitex/config/WebConfig.java
+++ src/main/java/br/senai/sc/communitex/config/WebConfig.java
@@ -9,9 +9,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        registry.addMapping("/**") //
+                .allowedOrigins(
+                        "https://communitex-front-x12.vercel.app",
+                        "http://localhost:3000"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
